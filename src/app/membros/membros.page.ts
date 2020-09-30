3c1b9b33a16e9f8e1b9e657b29f680da5382c5cd
wevertonwos08 <weverton_brasilia@hotmail.com>
2020-09-29 21:40:44 -0300

    Alteração do conteudo deputado (verificacão)

--- src/app/membros/membros.page.ts
+++ src/app/membros/membros.page.ts
@@ -1,8 +1,10 @@
+import { ModalDeputadoPage } from "./../modal-deputado/modal-deputado.page";
 import { Membro } from "./../models/membro";
 import { ApiService } from "./../services/api.service";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import {} from "../services/api.service";
+import { ModalController } from "@ionic/angular";
 import { Subscriber } from "rxjs";
 
 @Component({
@@ -11,18 +13,35 @@ import { Subscriber } from "rxjs";
   styleUrls: ["./membros.page.scss"],
 })
 export class MembrosPage implements OnInit {
-  membros: Array<Membro>;
+  membros: Array<Membro> = [];
   idPartido: number;
-  constructor(public router: ActivatedRoute, private apiserv: ApiService) {}
+  public loading: boolean = false;
+  constructor(
+    public router: ActivatedRoute,
+    private apiserv: ApiService,
+    public modal: ModalController
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     // console.log(this.router.snapshot.params.id);
+    this.loading = true;
     this.idPartido = this.router.snapshot.params.id;
-    this.buscarMembrosDoPratido(this.idPartido);
+    await this.buscarMembrosDoPratido(this.idPartido);
   }
   public buscarMembrosDoPratido(idPartido: number): void {
     this.apiserv.getMembros(this.idPartido).subscribe((response) => {
       this.membros = response.dados;
     });
+    this.loading = false;
+  }
+  async abrirModal(idDeputado: number) {
+    const modal = await this.modal.create({
+      component: ModalDeputadoPage,
+
+      componentProps: {
+        idDeputado,
+      },
+    });
+    return await modal.present();
   }
 }
